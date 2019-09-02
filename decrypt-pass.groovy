ec14122490a15c277def3342ef2ff9635570b4a0
Samit Kumar Patel <samitkumarpatel@users.noreply.github.com>
2019-09-02 09:38:31 +0200

    Update decrypt-pass.groovy

--- decrypt-pass.groovy
+++ decrypt-pass.groovy
@@ -1,2 +1,8 @@
-# {ENCRYPTED_PASSPHRASE_OR_PASSWORD} - this can be get from credential.xml
-println( hudson.util.Secret.decrypt("${ENCRYPTED_PASSPHRASE_OR_PASSWORD}") )
+# SECRET_TEXT - this can be get from credential.xml and it looks like {LOTS_OF_LETTER_NUMBER==}
+
+
+println(hudson.util.Secret.decrypt("SECRET_TEXT"))
+
+println(hudson.util.Secret.fromString("SECRET_TEXT").getPlainText())
+
+println(hudson.util.Secret.fromString("PASSWORD").getEncryptedValue())
